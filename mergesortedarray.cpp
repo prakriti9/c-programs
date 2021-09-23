d942e0bd8b1999cec2c4311ad68b23f085f413dc
Prakriti Agrawal <63722780+prakriti9@users.noreply.github.com>
2021-09-23 17:08:46 +0530

    merge

--- mergesortedarray.cpp
+++ mergesortedarray.cpp
@@ -1,57 +1,88 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// void merge(int ar1[], int ar2[], int m,int n)
+// {
+//     int ar3[m+n];
+//     int k = 0;
+//     int i = 0, j = 0;
+//     while(i < m and j <n)
+//     {
+//         if(ar1[i] < ar2[j])
+//         {
+//             ar3[k++] = ar1[i++];
+
+//         }
+//         else if(ar1[i] > ar2[j])
+//         {
+//             ar3[k++] = ar2[j++];
+            
+//         }
+//     }
+//     while(i<m)
+//     {
+//         ar3[k++] = ar1[i++]; 
+//     }
+//     while(j<m)
+//     {
+//         ar3[k++] = ar2[j++];
+//     }
+//     for(int i = 0;i<m+n;i++)
+//     {
+//         cout<<ar3[i]<<" ";
+//     }
+//     cout << endl;
+
+//     for(int i = 0;i<m;i++)
+//     {
+//         ar1[i] = ar3[i];
+//     }
+//         int l = 0;
+//         for(int j = m;j <= n;j++)
+//         {
+//             ar2[l++] = ar3[j];
+//         }
+//         for(int i = 0;i<m;i++)
+//         {
+//             cout<<ar1[i]<<" ";
+//         }
+//         cout<<endl;
+//         for(int i = 0;i<n;i++)
+//         {
+//             cout<<ar2[i]<<" ";
+//         }
+//         cout<<endl;
+// }
 void merge(int ar1[], int ar2[], int m,int n)
 {
-    int ar3[m+n];
-    int k = 0;
-    int i = 0, j = 0;
-    while(i < m and j <n)
+    // without using extra spaces
+    int i = m - 1;
+    int j = 0;
+    while(i>= 0 and j<n)
     {
-        if(ar1[i] < ar2[j])
+        if(ar1[i] >= ar2[j])
         {
-            ar3[k++] = ar1[i++];
-
+            swap(ar1[i],ar2[j]);
+            i--;
+            j++;
         }
-        else if(ar1[i] > ar2[j])
-        {
-            ar3[k++] = ar2[j++];
-            
+        else{
+            break;
         }
     }
-    while(i<m)
-    {
-        ar3[k++] = ar1[i++]; 
-    }
-    while(j<m)
-    {
-        ar3[k++] = ar2[j++];
-    }
-    for(int i = 0;i<m+n;i++)
-    {
-        cout<<ar3[i]<<" ";
-    }
-    cout << endl;
-
+    sort(ar1, ar1+m);
+    sort(ar2, ar2 + n);
+    // displaying the arrays
     for(int i = 0;i<m;i++)
     {
-        ar1[i] = ar3[i];
+        cout<<ar1[i]<<" ";
     }
-        int l = 0;
-        for(int j = m;j <= n;j++)
-        {
-            ar2[l++] = ar3[j];
-        }
-        for(int i = 0;i<m;i++)
-        {
-            cout<<ar1[i]<<" ";
-        }
-        cout<<endl;
-        for(int i = 0;i<n;i++)
-        {
-            cout<<ar2[i]<<" ";
-        }
-        cout<<endl;
+    cout<<endl;
+    for(int i = 0;i<n;i++)
+    {
+        cout<<ar2[i]<<" ";
+    }
+    cout<<endl;
 }
 int main()
 {
